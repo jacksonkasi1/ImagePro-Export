e803fe842e31b038cd904ddd4e6fc3f9459ac106
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-05-26 20:44:02 +0530

    Add support for path aliases in TypeScript and Webpack, and simplify App component to display Hello World

--- plugin/src/app/index.tsx
+++ plugin/src/app/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import  './styles/ui.css';
+import '@/styles/ui.css';
 
-import App from './components/App';
-
+import App from '@/components/App';
 
 document.addEventListener('DOMContentLoaded', function () {
   const container = document.getElementById('react-page');
