3f9159fbbdcbd2d6b618507d9c24fab71d3251d6
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-05-26 14:46:13 +0530

    chore: Add window resize functionality to plugin UI

--- plugin/src/main.ts
+++ plugin/src/main.ts
@@ -1,8 +1,17 @@
-import { showUI } from '@create-figma-plugin/utilities'
+import { on, showUI } from '@create-figma-plugin/utilities'
+
+import { ResizeWindowHandler } from '@/types/events'
 
 export default function () {
+  on<ResizeWindowHandler>(
+    'RESIZE_WINDOW',
+    function (windowSize: { width: number; height: number }) {
+      const { width, height } = windowSize
+      figma.ui.resize(width, height)
+    }
+  )
   showUI({
-    height: 160,
-    width: 240
+    height: 600,  // Initial height
+    width: 800    // Initial width
   })
 }
--- plugin/src/ui.tsx
+++ plugin/src/ui.tsx
@@ -1,8 +1,23 @@
-import { render } from '@create-figma-plugin/ui'
+import { render, useWindowResize } from '@create-figma-plugin/ui'
 import { h } from 'preact'
+import { emit } from '@create-figma-plugin/utilities'
 import '!./output.css'
 
-function Plugin () {
+import { ResizeWindowHandler } from '@/types/events'
+
+function Plugin() {
+  function onWindowResize(windowSize: { width: number; height: number }) {
+    emit<ResizeWindowHandler>('RESIZE_WINDOW', windowSize)
+  }
+  
+  useWindowResize(onWindowResize, {
+    maxHeight: 1000, // Increased maxHeight for flexibility
+    maxWidth: 1000,  // Increased maxWidth for flexibility
+    minHeight: 160,
+    minWidth: 640,
+    resizeBehaviorOnDoubleClick: 'minimize'
+  })
+
   return (
     <h1 class="text-3xl font-bold underline">
       Hello, World!
