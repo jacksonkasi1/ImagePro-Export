29fe724c42ef678f55bdb6a34ed676a69017cdbd
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-08-25 16:17:16 +0530

    re-name files to kebab-case

--- plugin/src/core/handlers/search-nodes-handler.ts
+++ plugin/src/core/handlers/search-nodes-handler.ts
@@ -2,7 +2,7 @@
 import { NodeData, NodeType } from '@/types/node';
 
 // ** import handlers
-import { getImageNodes } from './fetchImagesHandlers';
+import { getImageNodes } from './fetch-images-handlers';
 
 // Define the export size settings
 const exportSize = (type: 'SCALE' | 'HEIGHT', value: number): ExportSettings => ({
--- plugin/src/main.ts
+++ plugin/src/main.ts
@@ -2,7 +2,7 @@ import { showUI } from '@create-figma-plugin/utilities';
 
 export default function () {
   showUI({
-    height: 160,
-    width: 240,
+    height: 540, // min - 540
+    width: 320, // min - 320
   });
 }
