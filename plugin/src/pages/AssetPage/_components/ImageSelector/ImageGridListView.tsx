bc566976046ab80bbaffbb929dfd2bf79e83053e
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-08-31 00:02:06 +0530

    inclused HoverScrollbar & try to fix the chekcbox issue

--- plugin/src/pages/AssetPage/_components/ImageSelector/ImageGridListView.tsx
+++ plugin/src/pages/AssetPage/_components/ImageSelector/ImageGridListView.tsx
@@ -11,6 +11,7 @@ import { NodeData } from '@/types/node';
 
 // ** import utils
 import { cn, truncateText } from '@/lib/utils';
+import HoverScrollbar from '@/components/ui/hover-scrollbar';
 
 interface ImageGridListViewProps {
   allNodes: NodeData[];
@@ -30,95 +31,107 @@ const ImageGridListView = ({
   const isGridView = viewType === 'grid';
 
   return (
-    <div className={cn('gap-2 h-full max-h-80 overflow-y-scroll', isGridView ? 'grid grid-cols-2' : 'flex flex-col')}>
-      {allNodes.map((image, index) => (
-        <div
-          key={`${image.id}_${index}`}
-          className={cn(
-            'relative rounded-lg flex flex-col items-center cursor-pointer',
-            isGridView ? 'p-2' : 'px-2',
-            selectedNodeIds.includes(image.id) ? 'bg-selected-bg' : 'bg-secondary-bg'
-          )}
-          onClick={() => onSelectImage(image.id, !selectedNodeIds.includes(image.id))}
-        >
-          {isGridView ? (
-            <Fragment>
-              {/* Grid View */}
-              {/* Checkbox */}
-              <Checkbox
-                value={selectedNodeIds.includes(image.id)}
-                onValueChange={(checked) => onSelectImage(image.id, checked)}
-                className="absolute top-3 left-3"
-              />
-              {/* Image Preview */}
-              <div className={cn('overflow-hidden w-full h-fit max-h-20 flex justify-center items-center')}>
-                {base64Images[image.id] ? (
-                  <img
-                    src={base64Images[image.id]}
-                    alt={`${image.name}`}
-                    className={cn(
-                      'object-contain rounded-sm w-full h-full' // Object contain to keep the image within bounds
-                    )}
-                  />
-                ) : (
-                  <div
-                    className={cn(
-                      'rounded-sm w-full h-full' // Fixed size placeholder
-                    )}
-                  />
-                )}
-              </div>
-
-              {/* Image Name, Dimensions, Type */}
-              <div className={cn('flex flex-col gap-2 items-center mt-2')}>
-                <Columns space="medium">
-                  <div className="flex flex-col gap-1">
-                    <Text className={cn('truncate w-fit font-medium')}>{truncateText(image.name, 6)}</Text>
-                    <Text className="text-secondary-text">{image.type}</Text>
-                  </div>
-                  <Text className="text-secondary-text">
-                    {Math.round(image.dimensions.width)}x{Math.round(image.dimensions.height)}px
-                  </Text>
-                </Columns>
-              </div>
-            </Fragment>
-          ) : (
-            <Fragment>
-              {/* List View */}
-              <div className="flex items-center w-full gap-2">
+    <HoverScrollbar className="h-80">
+      <div className={cn('gap-2 h-full', isGridView ? 'grid grid-cols-2' : 'flex flex-col')}>
+        {allNodes.map((image, index) => (
+          <div
+            key={`${image.id}_${index}`}
+            className={cn(
+              'relative rounded-lg flex flex-col items-center cursor-pointer',
+              isGridView ? 'p-2' : 'px-2',
+              selectedNodeIds.includes(image.id) ? 'bg-selected-bg' : 'bg-secondary-bg'
+            )}
+            onClick={() => onSelectImage(image.id, !selectedNodeIds.includes(image.id))}
+          >
+            {isGridView ? (
+              <Fragment>
+                {/* Grid View */}
                 {/* Checkbox */}
                 <Checkbox
                   value={selectedNodeIds.includes(image.id)}
-                  onValueChange={(checked) => onSelectImage(image.id, checked)}
-                  className="self-center"
+                  onValueChange={(checked, event) => {
+                    event.stopPropagation(); // Prevent the click event from propagating to the parent div
+                    onSelectImage(image.id, checked);
+                  }}
+                  className="absolute cursor-pointer top-3 left-3"
                 />
                 {/* Image Preview */}
-                <div className="flex items-center justify-center w-16 h-16 overflow-hidden cursor-pointer">
+                <div className={cn('overflow-hidden w-full h-fit max-h-20 flex justify-center items-center cursor-pointer')}>
                   {base64Images[image.id] ? (
-                    <img src={base64Images[image.id]} alt={`${image.name}`} className="object-contain w-full h-full" />
+                    <img
+                      src={base64Images[image.id]}
+                      alt={`${image.name}`}
+                      className={cn(
+                        'object-contain rounded-sm w-full h-full' // Object contain to keep the image within bounds
+                      )}
+                    />
                   ) : (
-                    <div className="w-full h-full bg-gray-200" />
+                    <div
+                      className={cn(
+                        'rounded-sm w-full h-full' // Fixed size placeholder
+                      )}
+                    />
                   )}
                 </div>
 
-                {/* Image Details */}
-                <div className="flex flex-row items-center justify-between w-full gap-2">
-                  <div className="flex flex-col ">
-                    <Text className={cn('truncate w-full font-medium')}>{truncateText(image.name, 17)}</Text>
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <Text className="text-secondary-text">{image.type}</Text>
+                {/* Image Name, Dimensions, Type */}
+                <div className={cn('flex flex-col gap-2 items-center mt-2')}>
+                  <Columns space="medium">
+                    <div className="flex flex-col gap-1">
+                      <Text className={cn('truncate w-fit font-medium')}>{truncateText(image.name, 6)}</Text>
+                      <Text className="text-secondary-text">{image.type}</Text>
+                    </div>
                     <Text className="text-secondary-text">
                       {Math.round(image.dimensions.width)}x{Math.round(image.dimensions.height)}px
                     </Text>
+                  </Columns>
+                </div>
+              </Fragment>
+            ) : (
+              <Fragment>
+                {/* List View */}
+                <div className="flex items-center w-full gap-2 cursor-pointer">
+                  {/* Checkbox */}
+                  <Checkbox
+                    value={selectedNodeIds.includes(image.id)}
+                    onValueChange={(checked, event) => {
+                      event.stopPropagation(); // Prevent the click event from propagating to the parent div
+                      onSelectImage(image.id, checked);
+                    }}
+                    className="self-center"
+                  />
+                  {/* Image Preview */}
+                  <div className="flex items-center justify-center w-16 h-16 overflow-hidden cursor-pointer">
+                    {base64Images[image.id] ? (
+                      <img
+                        src={base64Images[image.id]}
+                        alt={`${image.name}`}
+                        className="object-contain w-full h-full"
+                      />
+                    ) : (
+                      <div className="w-full h-full bg-gray-200" />
+                    )}
+                  </div>
+
+                  {/* Image Details */}
+                  <div className="flex flex-row items-center justify-between w-full gap-2">
+                    <div className="flex flex-col ">
+                      <Text className={cn('truncate w-full font-medium')}>{truncateText(image.name, 17)}</Text>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <Text className="text-secondary-text">{image.type}</Text>
+                      <Text className="text-secondary-text">
+                        {Math.round(image.dimensions.width)}x{Math.round(image.dimensions.height)}px
+                      </Text>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Fragment>
-          )}
-        </div>
-      ))}
-    </div>
+              </Fragment>
+            )}
+          </div>
+        ))}
+      </div>
+    </HoverScrollbar>
   );
 };
 
