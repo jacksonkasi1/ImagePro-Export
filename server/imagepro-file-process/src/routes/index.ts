205481b798af451c2dece259cda5e224cb423b1e
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-09-28 22:24:41 +0530

    incluse the file process api

--- server/imagepro-file-process/src/routes/index.ts
+++ server/imagepro-file-process/src/routes/index.ts
@@ -5,6 +5,7 @@ import cmkyRouter from "./pdf-opt/cmky";
 import passwordRouter from "./pdf-opt/password";
 import grayscaleRouter from "./pdf-opt/grayscale";
 import mergeRouter from "./pdf-opt/merge";
+import pdfProcessRouter from "./pdf-opt/process";
 
 const router = Router();
 
@@ -13,5 +14,7 @@ router.use("/pdf-opt", cmkyRouter);
 router.use("/pdf-opt", passwordRouter);
 router.use("/pdf-opt", grayscaleRouter);
 router.use("/pdf-opt", mergeRouter);
+router.use("/pdf-opt", pdfProcessRouter)
+
 
 export default router;
