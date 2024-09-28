7a1fe542295692555c661a50f76703461ca38d8a
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-09-28 23:38:51 +0530

    update the config link

--- plugin/src/config/index.ts
+++ plugin/src/config/index.ts
@@ -1,4 +1,3 @@
 export const config = {
-  // FILE_SERVER: 'https://jacksonkasi-imagepro-file-process.hf.space',
-  FILE_SERVER: 'https://7860-jacksonkasi-imageproexp-oiiwh1hrty6.ws-us116.gitpod.io',
+  FILE_SERVER: 'https://jacksonkasi-imagepro-file-process.hf.space',
 };
