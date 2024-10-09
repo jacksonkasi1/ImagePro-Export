74cbd6e802f99d95e79ea71daf8737de5fd2da5c
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-10-10 00:00:47 +0530

    added PINATA_GATEWAY

--- plugin/src/config/index.ts
+++ plugin/src/config/index.ts
@@ -1,4 +1,5 @@
 export const config = {
+  PINATA_GATEWAY: 'https://lime-casual-meerkat-982.mypinata.cloud',
   // FILE_SERVER: 'https://jacksonkasi-imagepro-file-process.hf.space',
   FILE_SERVER: 'https://7860-jacksonkasi-imageproexp-aynsah4e7hn.ws-us116.gitpod.io', // for testing
 };
