fd9003d7fae939fe049d87c07d30e4bde7dca2d8
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-10-11 01:31:52 +0530

    update config

--- plugin/src/config/index.ts
+++ plugin/src/config/index.ts
@@ -1,6 +1,6 @@
 export const config = {
   PINATA_GATEWAY: 'https://lime-casual-meerkat-982.mypinata.cloud',
-  // FILE_SERVER: 'https://jacksonkasi-imagepro-file-process.hf.space',
-  FILE_SERVER: 'http://localhost:7860', // for testing
-  PDF_LOGO: 'https://cdn.file0.dev/ihbyf1o3qp3rtpmhoik29mdq.png'
+  PDF_LOGO: 'https://cdn.file0.dev/ihbyf1o3qp3rtpmhoik29mdq.png',
+  FILE_SERVER: 'https://jacksonkasi-imagepro-file-process.hf.space',
+  // FILE_SERVER: 'http://localhost:7860', // for testing
 };
