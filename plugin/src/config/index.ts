d89435dea5ed1cbb8a985577774be30fb6029aea
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-10-17 22:57:45 +0530

    update the pdf logo cdn link

--- plugin/src/config/index.ts
+++ plugin/src/config/index.ts
@@ -1,6 +1,6 @@
 export const config = {
   PINATA_GATEWAY: 'https://lime-casual-meerkat-982.mypinata.cloud',
-  PDF_LOGO: 'https://lime-casual-meerkat-982.mypinata.cloud/files/bafkreihcpic2x3m5bbd5hpcxmba2hbxw2lzjugnqmbf6dtea2v6t5jnzli',
+  PDF_LOGO: 'https://cdn.file0.dev/ihbyf1o3qp3rtpmhoik29mdq.png',
   FILE_SERVER: 'https://jacksonkasi-imagepro-file-process.hf.space',
   // FILE_SERVER: 'http://localhost:7860', // for testing
 };
